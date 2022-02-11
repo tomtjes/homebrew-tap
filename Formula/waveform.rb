875dddef370f6f0f2763300a1856ff50bab8371f
tomtjes <nesiofnqwe@mailbox.org>
2022-02-10 22:18:55 -0500

    make executable

--- Formula/waveform.rb
+++ Formula/waveform.rb
@@ -1,34 +1,14 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Waveform < Formula
-  desc "Uploads transactions from a csv file to a waveapps.com account."
+  desc "Uploads transactions from a csv file to a waveapps.com account"
   homepage "https://github.com/tomtjes/waveform"
-  url "https://github.com/tomtjes/waveform/archive/refs/tags/v0.3.tar.gz"
-  sha256 "2dd848777297a5147adaf3ab292f15438f6ecdeb6fb45211670d4013eb878b6b"
+  url "https://github.com/tomtjes/waveform/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "be62339d49cffb0b3d4bc3158348f94500f3bdb0a1df2e7a57d13b70f142f58b"
   license "MIT"
 
   depends_on "python@3.9"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    # system "./configure", *std_configure_args, "--disable-silent-rules"
     bin.install "waveform.py" => "waveform"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
   end
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test waveform`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
-  end
 end
