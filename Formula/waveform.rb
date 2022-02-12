e858720a9e8a9ef229dbc09505f9f0f406075ee3
tomtjes <nesiofnqwe@mailbox.org>
2022-02-11 20:02:55 -0500

    v0.3.2

--- Formula/waveform.rb
+++ Formula/waveform.rb
@@ -1,15 +1,23 @@
 class Waveform < Formula
   desc "Uploads transactions from a csv file to a waveapps.com account"
   homepage "https://github.com/tomtjes/waveform"
-  url "https://github.com/tomtjes/waveform/archive/refs/tags/v0.3.1.tar.gz"
-  sha256 "be62339d49cffb0b3d4bc3158348f94500f3bdb0a1df2e7a57d13b70f142f58b"
+  url "https://github.com/tomtjes/waveform/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "b5ca11d74fd7c7221da7963aeb31a879cda016b14f2fbae6583200ed89971e61"
   license "MIT"
 
   depends_on "python@3.9"
 
   def install
     bin.install "waveform.py" => "waveform"
-    prefix.install "globals.py", "helpers.py", "post.py", "search.py", "setup.py", "wave_api.py"
+    prefix.install "waveform_globals.py",
+      "waveform_helpers.py",
+      "waveform_post.py",
+      "waveform_search.py",
+      "waveform_setup.py",
+      "waveform_wave.py"
   end
 
+  test do
+    system "false"
+  end
 end
